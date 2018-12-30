090052267935a1fc8fc1e6e3c54ca27455f29caa
铁腕阿童木 <zhengzehao@maka.im>
2018-12-30 23:08:57 +0800

    使用request进行表单验证

--- app/Article.php
+++ app/Article.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model {
     protected $fillable = ['title', 'content', 'published_at'];
-
+    protected $dates = ['published_at'];
     //set Title(对应上面的title) Attribute
     public function setPublishedAtAttribute($date) {
         //会把传入的$date用carbon转化再存入数据库
@@ -15,5 +15,10 @@ class Article extends Model {
         $month = date("m",strtotime($date));
         $day = date("d",strtotime($date));
         $this->attributes['published_at'] = Carbon::createFromDate($year,$month,$day);
+    }
+
+    //scope关键字+函数名字(驼峰法)
+    public function scopePublished($query){
+       $query->where('published_at','<=',Carbon::now());
     }
 }
