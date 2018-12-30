f14d04ec19ee764a14b501cf78366fccc65d27e7
铁腕阿童木 <zhengzehao@maka.im>
2018-12-30 20:21:09 +0800

    setAttribute预处理数据

--- app/Article.php
+++ app/Article.php
@@ -2,10 +2,18 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
-class Article extends Model
-{
-    //
-    protected $fillable = ['title','content','published_at'];
+class Article extends Model {
+    protected $fillable = ['title', 'content', 'published_at'];
+
+    //set Title(对应上面的title) Attribute
+    public function setPublishedAtAttribute($date) {
+        //会把传入的$date用carbon转化再存入数据库
+        $year = date("Y",strtotime($date));
+        $month = date("m",strtotime($date));
+        $day = date("d",strtotime($date));
+        $this->attributes['published_at'] = Carbon::createFromDate($year,$month,$day);
+    }
 }
